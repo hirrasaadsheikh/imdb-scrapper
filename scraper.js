d96bfce6d03f392c82bee3c147f67f0230ffb3f8
unknown <hirrasaadsheikh@gmail.com>
2022-11-23 16:08:39 +0500

    first commit

---
+++ scraper.js
@@ -0,0 +1,25 @@
+const puppeteer = require("puppeteer");
+
+function delay(time) {
+    return new Promise(function (resolve) {
+        setTimeout(resolve, time)
+    });
+}
+async function imdb (){
+    try {
+        const browser = await puppeteer.launch({headless: false, slowMo:10});
+        const page = await browser.newPage();
+        await page.goto("https://www.imdb.com/", { waitUntil: 'load', timeout: 0 });
+        console.log('Welcome to IMDb Page');
+        await delay(5000);
+
+        await page.waitForSelector("#suggestion-search");
+        await page.type("#suggestion-search","The Office", {delay: 20});
+        console.log('Clicked');
+        await page.click("#suggestion-search-button");
+    }
+    catch (e) {
+        console.log("Unable to extract data", e);
+    }
+
+} imdb();
